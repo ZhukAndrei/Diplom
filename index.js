eeb7d114fb45fc641626e460db88c60716185c4e
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2022-01-16 17:05:35 +0300

    keyEnter url request

--- index.js
+++ index.js
@@ -1,64 +1,11 @@
-//console.log(createUrl(url, inputSearchValue))
+sum2Parameter()
+//Ввод URL по клику на кнопке
+submit.addEventListener("click", createUrl) 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//===================================================================
-// let inputSearch = document.getElementById('inputSearch')
-// let url
-// let submit = document.getElementById("submit");
-// url = (`https://deezerdevs-deezer.p.rapidapi.com/search?q=${inputSearch.value}`)
-
-// submit.addEventListener("click", function(){ 
-//     alert(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${inputSearch.value}`);
-//     //url = (`https://deezerdevs-deezer.p.rapidapi.com/search?q=${inputSearch.value}`);
-
-    
-//      }); 
-
-// //console.log(url)
-// fetch(url, {
-	
-// // fetch("https://api.deezer.com/search?q=eminem", {
-// "method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-// 		"x-rapidapi-key": "63139df467msh62775601a5f1143p1aa727jsna2352e2ddc8e"
-// 	}
-// })
-//   .then(response => response.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
-
-
-
-// //   submit.addEventListener("click", () => {
-// //     if (document.querySelector("#submit").value) {
-// //         createUrl()
-// //     }
-//      console.log(url) 
-// // });
-
-// //console.log(createUrl()) 
+//Ввод URL по кнопке Enter
+inputSearchValue.addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) {
+        createUrl()
+        //console.log(this.value);
+    }
+  });
