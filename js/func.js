a3ef202bf1b643e95580cf04140eef1cf4b053d5
ZhukAndrei <93341242+ZhukAndrei@users.noreply.github.com>
2022-01-28 11:57:27 +0300

    corrected play list function

--- js/func.js
+++ js/func.js
@@ -18,7 +18,7 @@ function createUrl() {
     .then((response) => response.json())
     .then((json) => {
       json.data.forEach((element) => {
-        // создаем массив данных для вывода в трек-лист проигрывания 
+        // создаем массив данных для вывода в трек-лист проигрывания
         arrTreckList.push({
           artistName: element.artist.name,
           albumName: element.album.title,
@@ -27,56 +27,60 @@ function createUrl() {
         });
       });
     })
-    .catch(err => {
+    .catch((err) => {
       //console.error(err);
       // выводим сообщение об ошибке, если превышен лимит запросов в единицу времени к АПИ
-      alert ("Ошибка запроса. Превышено количество запросов в ДЕМО-АПИ. Повторите запрос.")
+      alert(
+        "Ошибка запроса. Превышено количество запросов в ДЕМО-АПИ. Повторите запрос."
+      );
     });
 
-// создаем шаблон ячейки музыкальной композиции с дополнительной информацией
-generateTreckList = (artistName, albumName, songName, songPreview) => {
-   return `<div class = "pSrc" value = '${songPreview}'>
+  // создаем шаблон ячейки музыкальной композиции с дополнительной информацией
+  generateTreckList = (artistName, albumName, songName, songPreview) => {
+    return `<div class = "pSrc" value = '${songPreview}'>
   <li>
   Artist: <strong>${artistName}</strong> <br>
   Album: <strong>${albumName}</strong> <br>
-  Song: <strong>${songName}</strong></li> <br> </div>`
-}
+  Song: <strong>${songName}</strong></li> <br> </div>`;
+  };
 
-generateMp3List = (songPreview) => {
-  return `<source src="${songPreview}" type="audio/mpeg" />`
-}
+  generateMp3List = (songPreview) => {
+    return `<source src="${songPreview}" type="audio/mpeg" />`;
+  };
 
-// =========заполняем шаблон ячейки музыкальной композиции
-mp3ListHTML = arrTreckList.map((mp3) => {
-  return generateMp3List(
-  mp3.songPreview
-  );
-})
-.join('');
+  // =========заполняем шаблон ячейки музыкальной композиции
+  mp3ListHTML = arrTreckList
+    .map((mp3) => {
+      return generateMp3List(mp3.songPreview);
+    })
+    .join("");
 
-treckListHTML = arrTreckList.map((informSong) => {
-  return generateTreckList(
-    informSong.artistName,
-    informSong.albumName,
-    informSong.songName,
-    informSong.songPreview
-  );
-})
-.join('');
-  
-if (ulTreckList.li != "") {
-  arrTreckList.length = 0;
-  document.querySelector('#treck').innerHTML = '';
-} 
-ulTreckList.innerHTML = treckListHTML;
-console.log("treckListHTML: ", treckListHTML);
-document.querySelector('#treck').innerHTML = mp3ListHTML;
-console.log("mp3ListHTML: ", mp3ListHTML);
+  treckListHTML = arrTreckList
+    .map((informSong) => {
+      return generateTreckList(
+        informSong.artistName,
+        informSong.albumName,
+        informSong.songName,
+        informSong.songPreview
+      );
+    })
+    .join("");
+
+  if (ulTreckList.li != "") {
+    arrTreckList.length = 0;
+    document.querySelector("#treck").innerHTML = "";
+  }
+  ulTreckList.innerHTML = treckListHTML;
+  console.log("treckListHTML: ", treckListHTML);
+  document.querySelector("#treck").innerHTML = mp3ListHTML;
+  console.log("mp3ListHTML: ", mp3ListHTML);
 }
 // проверяем создание массива
-console.log('arrTrecklist: ', arrTreckList);
+console.log("arrTrecklist: ", arrTreckList);
 
 //делегируем события списка песен
+
+choiceAudio = new Audio();
 //ЗАПУСКАЕТ НО НЕ ВЫКЛЮЧАЕТ!!!! ====================================
 var choiceAudio;
 ulTreckList.onclick = (e) => {
@@ -87,37 +91,58 @@ ulTreckList.onclick = (e) => {
     let srcRef = srcDiv.getAttribute("value");
     console.log("srcRef", srcRef);
     controlBtn.innerText = "Play";
-    choiceAudio = new Audio(srcRef);
-    choiceAudio.play();
-    playPause()
+    choiceAudio.src = srcRef;
+    if (treck.paused) {
+      treck.play();
+      controlBtn.innerText = "Pause";
+    } else {
+      treck.pause();
+      controlBtn.innerText = "Play";
+    }
+
+    treckPlayList = choiceAudio.play();
+    playPause();
   }
 
-  function playPause() {
-      audioPlay = setInterval(function () {
-        // Получаем значение на какой секунде песня
-        let audioTime = Math.round(treck.currentTime);
-        // Получаем всё время песни
-        let audioLength = Math.round(treck.duration);
-        // Назначаем ширину элементу time
-        time.style.width = (audioTime * 100) / audioLength + "%";
-      }, 10);
-      if (treck.paused) {
-        treck.play();
-        controlBtn.innerText = "Pause";
-      } else {
-        treck.pause();
-        controlBtn.innerText = "Play";
-      }
-    }
-    
-
-
-};
-  document.querySelector("#volume").onclick = audioVolume;
-function audioVolume () {
-  let v = this.value
-treck.volume = v / 100;
-choiceAudio.volume = v / 100;
 }
 
+let treckPlayList;
 
+
+//   function playPause() {
+//     audioPlay = setInterval(function () {
+//       // Получаем значение на какой секунде песня
+//       let audioTime = Math.round(treck.currentTime);
+//       // Получаем всё время песни
+//       let audioLength = Math.round(treck.duration);
+//       // Назначаем ширину элементу time
+//       time.style.width = (audioTime * 100) / audioLength + "%";
+//     }, 10);
+//     if (treck.paused) {
+//       treck.play();
+//       controlBtn.innerText = "Pause";
+//     } else {
+//       treck.pause();
+//       controlBtn.innerText = "Play";
+//     }
+//   }
+// };
+document.querySelector("#volume").onclick = audioVolume;
+function audioVolume() {
+  let v = this.value;
+  choiceAudio.volume = v / 100;
+}
+
+// controlBtn.addEventListener("click", playPause);
+// treck.addEventListener("ended", function () {
+//   controlBtn.innerText = "Play";
+// });
+
+// btnStop.addEventListener("click", function () {
+//   treck.pause();
+//   treck.currentTime = 0;
+//   controlBtn.innerText = "Play";
+// });
+// treck.addEventListener("ended", function () {
+//   controlBtn.innerText = "Play";
+// });
--- js/player.js
+++ js/player.js
@@ -1,10 +1,10 @@
-let treck = document.getElementById('treck');
-let controlBtn = document.getElementById('play-pause');
-let btnPrev = document.querySelector("#prev");   // Берём кнопку переключения предыдущего трека
-let btnNext = document.querySelector("#next");   // Берём кнопку переключение следующего трека
-let btnStop = document.querySelector("#stop");   // Берём кнопку переключение следующего трека
-let time = document.querySelector('.time');
-
+let treck = document.getElementById("treck");
+let controlBtn = document.getElementById("play-pause");
+let btnPrev = document.querySelector("#prev"); // Берём кнопку переключения предыдущего трека
+let btnNext = document.querySelector("#next"); // Берём кнопку переключение следующего трека
+let btnStop = document.querySelector("#stop"); // Берём кнопку переключение следующего трека
+let time = document.querySelector(".time");
+let audioPlay;
 let treckNumber;
 // let playlist = [
 //     '1.mp3',
@@ -13,39 +13,35 @@ let treckNumber;
 //     './audio/4.mp3',
 // ];
 
-
 // Play/Pause функция =============================
 function playPause() {
-  audioPlay = setInterval(function() {
+  audioPlay = setInterval(function () {
     // Получаем значение на какой секунде песня
     let audioTime = Math.round(treck.currentTime);
     // Получаем всё время песни
-    let audioLength = Math.round(treck.duration)
+    let audioLength = Math.round(treck.duration);
     // Назначаем ширину элементу time
-    time.style.width = (audioTime * 100) / audioLength + '%';
-}, 10)
-   if (treck.paused) {
-        treck.play();
-        controlBtn.innerText = "Pause";
-    } else { 
-        treck.pause();
-        controlBtn.innerText = "Play";
-        audioPlay = ''
-    }
-
+    time.style.width = (audioTime * 100) / audioLength + "%";
+  }, 10);
+  if (treck.paused) {
+    treck.play();
+    controlBtn.innerText = "Pause";
+  } else {
+    treck.pause();
+    controlBtn.innerText = "Play";
+  }
 }
 //============================================
 controlBtn.addEventListener("click", playPause);
-treck.addEventListener("ended", function() {
+treck.addEventListener("ended", function () {
   controlBtn.innerText = "Play";
 });
 
 btnStop.addEventListener("click", function () {
-    treck.pause()
-    treck.currentTime = 0;
-    controlBtn.innerText = "Play";
-  
+  treck.pause();
+  treck.currentTime = 0;
+  controlBtn.innerText = "Play";
 });
-treck.addEventListener("ended", function() {
+treck.addEventListener("ended", function () {
   controlBtn.innerText = "Play";
 });
